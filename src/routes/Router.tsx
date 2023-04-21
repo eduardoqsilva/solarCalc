6c2dfde04d5f914d14c911c4ed413db35abfbfa1
eduardoqsilva <eduadsilva@outlook.com>
2023-04-21 17:25:11 -0300

    create a new route home

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { CoordinatesForm } from "../pages/CoordinatesForm";
+import { Home } from "../pages/Home";
 
 export function Router() {
   return (
     <Routes>
       <Route path={'/'} element={<CoordinatesForm />} />
+      <Route path={'/home'} element={<Home />} />
     </Routes>
   )
 }
