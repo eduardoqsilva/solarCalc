38596fd4b3400c8abd934c5eb4efa574bf0ff277
eduardoqsilva <eduadsilva@outlook.com>
2023-04-23 15:37:16 -0300

    update home

--- src/pages/Home/home.styled.ts
+++ src/pages/Home/home.styled.ts
@@ -1,1 +1,89 @@
 import styled from "styled-components";
+
+export const ContainerStyled = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  padding: 0 1.5rem 1.5rem 1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  
+  overflow-x: hidden;
+
+  @media (max-width: 654px) {
+    align-items: flex-start;
+  }
+`
+
+export const ContentContainer = styled.main`
+  width: 100%;
+  min-height: 100%;
+  max-width: 950px;
+  padding-top: 100px;
+
+  /* @media (max-width: 654px) {
+    padding-top: 0;
+  } */
+`
+export const GridStyled = styled.div`
+  width: 100%;
+  min-height: 100%;
+  
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: repeat(3,270px);
+  gap: 0.5rem;
+  
+  position: relative;
+  z-index: 10;
+  @media (max-width: 964px) {
+    grid-template-rows: auto 270px 270px;
+  }
+  @media (max-width: 654px) {
+    gap: 1rem;
+  }
+`
+export const CardsGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 1fr 1fr;
+  gap: 0.5rem;
+
+  @media (max-width: 964px) {
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+  }
+  @media (max-width: 654px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+  @media (max-width: 650px) {
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: repeat(1, 1fr);
+    overflow-x: scroll;
+    gap: 1rem;
+  }
+`
+export const OrnamentsStyled = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 0;
+
+  width: 100%;
+  height: 100%;
+
+  & img.up {
+    position: absolute;
+    bottom: 10%;
+    left: 41px;
+  }
+  & img.down {
+    position: absolute;
+    top: 10%;
+    right: 41px;
+  }
+
+  @media (max-width: 1200px) {
+    display: none
+  }
+`
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,97 @@
+import { BoundingBox, LightbulbFilament, Lightning, PottedPlant, SunDim, Thermometer } from "@phosphor-icons/react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { Card } from "../../components/Card";
+import { BarChart, LineChart } from "../../components/Charts";
+import { Loading } from "../../components/Loading";
+import { Logo } from "../../components/Logo";
+import { CardsGrid, ContainerStyled, ContentContainer, GridStyled, OrnamentsStyled } from "./home.styled";
 
+import up from '../../assets/svgs/decal/up.svg'
+import down from '../../assets/svgs/decal/down.svg'
+
+
+const labels = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez']
+const dat = [100, 200, 300, 400, 540, 700, 500, 300, 400, 500, 600, 790]
 
 export function Home() {
+  const { lat, long, cons } = useParams();
+  const [isLoading, setIsLoading] = useState(false)
+  const [data, setData] = useState({
+    dataBar: {
+      data: [],
+      labels: []
+    },
+    dataLine: {
+      data: [],
+      labels: []
+    },
+    pot: 0,
+    ger_anual: 0,
+    area: 0,
+    irradiacao: 0,
+    temp: 0,
+    co2: 0
+  })
+
   return (
-    <h1>home</h1>
+    <>
+      <Logo />
+      <OrnamentsStyled>
+        <img src={up} className="up" />
+        <img src={down}className="down" />
+      </OrnamentsStyled>
+      <Loading show={isLoading} variant={"01"}/>
+      <ContainerStyled>
+        <ContentContainer>
+          <GridStyled>
+            <CardsGrid>
+              <Card
+                title={'Potência do total sistema'}
+                icon={<Lightning/>}
+                value={['216.8', 'kW']}
+              />
+               <Card
+                title={'Geração anual'}
+                icon={<LightbulbFilament/>}
+                value={['216.8', 'kWh']}
+              />
+               <Card
+                title={'Área de instalação'}
+                icon={<BoundingBox/>}
+                value={['216.8', 'm²']}
+              />
+               <Card
+                title={'Média irradiação anual'}
+                icon={<SunDim/>}
+                value={['5559.56', 'W/m²']}
+              />
+               <Card
+                title={'Temperatura média anual'}
+                icon={<Thermometer/>}
+                value={['21.85', 'C°']}
+              />
+               <Card
+                title={'Redução de CO2'}
+                icon={<PottedPlant/>}
+                value={['3.6', 'TON']}
+              />
+            </CardsGrid>
+            <BarChart 
+              dat={dat}
+              labels={labels}
+              title={'Geração anual mensal em kWh'} 
+              dataLegend={'kWh'}            
+            />
+            <LineChart 
+              dat={dat}
+              labels={labels}
+              title={'Média irradiação mensal em W/m²'}
+              dataLegend={'W/m²'}
+            />
+          </GridStyled>
+        </ContentContainer>
+      </ContainerStyled>
+    </>
   )
 }
