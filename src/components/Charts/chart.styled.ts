b14e4597cdbe451c1c3f10cf5989f0b01673e895
eduardoqsilva <eduadsilva@outlook.com>
2023-04-23 15:42:25 -0300

    Create a component chart

---
+++ src/components/Charts/chart.styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { colors } from "../../styles/variables";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: ${colors.gray2};
+  border-radius: 8px;
+
+  padding: 0.5rem 1rem;
+`
