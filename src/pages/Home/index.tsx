3d3b0d808db5bca272d7cc6d92b5f2370f247994
eduardoqsilva <eduadsilva@outlook.com>
2023-04-23 21:45:13 -0300

    add data

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,38 +1,70 @@
 import { BoundingBox, LightbulbFilament, Lightning, PottedPlant, SunDim, Thermometer } from "@phosphor-icons/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Card } from "../../components/Card";
 import { BarChart, LineChart } from "../../components/Charts";
 import { Loading } from "../../components/Loading";
 import { Logo } from "../../components/Logo";
 import { CardsGrid, ContainerStyled, ContentContainer, GridStyled, OrnamentsStyled } from "./home.styled";
+import getData from "../../constants/solarCalcfunctions";
 
 import up from '../../assets/svgs/decal/up.svg'
 import down from '../../assets/svgs/decal/down.svg'
 
+interface dataType {
+  mediaIrradiacaoAnual: string,
+  mediaPotCorrigida: number,
+  mediaTemperaturaAnual: string,
+  qtdModulos: number,
+  potSistema: number,
+  energiaGeradaArray: {
+    meses: string[],
+    dados: string[]
+  },
+  mediaIrradiacao: {
+    meses: string[],
+    dados: string[]
+  },
+  mediaTemp: {
+    meses: string[],
+    dados: string[]
+  },
+  energiaTotal: string,
+  co2: string,
+  arvores: number,
+  areaInstalacao: string
+}
 
-const labels = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez']
-const dat = [100, 200, 300, 400, 540, 700, 500, 300, 400, 500, 600, 790]
 
 export function Home() {
   const { lat, long, cons } = useParams();
   const [isLoading, setIsLoading] = useState(false)
-  const [data, setData] = useState({
-    dataBar: {
-      data: [],
-      labels: []
-    },
-    dataLine: {
-      data: [],
-      labels: []
-    },
-    pot: 0,
-    ger_anual: 0,
-    area: 0,
-    irradiacao: 0,
-    temp: 0,
-    co2: 0
-  })
+  const [data, setData] = useState<dataType>()
+  const co2F = () => {
+    if(data) {
+      if(typeof data.co2 === 'string') {
+        const num = Number(data.co2)
+        return (num / 1000).toFixed(1)
+      }
+    }
+  }
+
+  useEffect(() => {
+    setIsLoading(true)
+    getData(Number(lat), Number(long), Number(cons))
+    .then((dat:any) => {
+      setData({
+        ...dat
+      })
+      setIsLoading(false)
+    }).catch((err) => {
+      console.error(err)
+    })
+  }, [ lat, long, cons ])
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
 
   return (
     <>
@@ -49,43 +81,53 @@ export function Home() {
               <Card
                 title={'Potência do total sistema'}
                 icon={<Lightning/>}
-                value={['216.8', 'kW']}
+                value={[`${data ? data?.potSistema : ''}`, 'kW']}
               />
                <Card
                 title={'Geração anual'}
                 icon={<LightbulbFilament/>}
-                value={['216.8', 'kWh']}
+                value={[`${data ? data?.energiaTotal : ''}`, 'kWh']}
               />
                <Card
                 title={'Área de instalação'}
                 icon={<BoundingBox/>}
-                value={['216.8', 'm²']}
+                value={[`${data ? data?.areaInstalacao : ''}`, 'm²']}
               />
                <Card
                 title={'Média irradiação anual'}
                 icon={<SunDim/>}
-                value={['5559.56', 'W/m²']}
+                value={[`${data ? data?.mediaIrradiacaoAnual : ''}`, 'W/m²']}
               />
                <Card
                 title={'Temperatura média anual'}
                 icon={<Thermometer/>}
-                value={['21.85', 'C°']}
+                value={[`${data ? data?.mediaTemperaturaAnual : ''}`, 'C°']}
               />
                <Card
                 title={'Redução de CO2'}
                 icon={<PottedPlant/>}
-                value={['3.6', 'TON']}
+                value={[`${co2F()}`, 'TON']}
               />
             </CardsGrid>
             <BarChart 
-              dat={dat}
-              labels={labels}
+              dat={data?.energiaGeradaArray.dados ?
+                     data?.energiaGeradaArray.dados.map((i) => {
+                       return parseFloat(i)
+                     }) 
+                     : []
+                  }
+              labels={data?.energiaGeradaArray.meses ? data?.energiaGeradaArray.meses : [] }
               title={'Geração anual mensal em kWh'} 
               dataLegend={'kWh'}            
             />
             <LineChart 
-              dat={dat}
-              labels={labels}
+              dat={data?.mediaIrradiacao.dados ?
+                data?.mediaIrradiacao.dados.map((i) => {
+                  return parseFloat(i)
+                }) 
+                : []
+             }
+              labels={data?.mediaIrradiacao.meses ? data?.mediaIrradiacao.meses : []}
               title={'Média irradiação mensal em W/m²'}
               dataLegend={'W/m²'}
             />
