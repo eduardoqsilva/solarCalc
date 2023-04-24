45099341f21d9ba7c5359bc9b7265342f401205b
FaSnow <eng.fbneves@gmail.com>
2023-04-23 22:28:46 -0300

    Reorganizando os dados !

--- src/constants/solarCalcFunction-ts/index.ts
+++ src/constants/solarCalcFunction-ts/index.ts
@@ -6,7 +6,6 @@ import Painel from './solarPanel';
 const painel = Painel[1];
 
 
-
 interface TotalInfo {
     mediaIrradiacaoAnual: number;
     mediaPotCorrigida: number;
@@ -17,7 +16,7 @@ interface TotalInfo {
     mediaIrradiacao: { meses: string[]; dados: number[] };
     mediaTemp: { meses: string[]; dados: number[] };
     energiaTotal: number;
-    co2: number;
+    TONco2: number;
     arvores: number;
     areaInstalacao: number;
 }
@@ -32,7 +31,7 @@ const totalInfo: TotalInfo = {
     mediaIrradiacao: { meses: [], dados: [] },
     mediaTemp: { meses: [], dados: [] },
     energiaTotal: 0,
-    co2: 0,
+    TONco2: 0,
     arvores: 0,
     areaInstalacao: 0,
 };
@@ -42,7 +41,6 @@ async function getData(latitude: number, longitude: number, consumo: number) {
     const openMeteo = new OpenMeteo(latitude, longitude);
     try {
         await openMeteo.addMedia();
-
 
         const mediaIrradiacaoAnual = OpenMeteo.meses.reduce((total, mes) => {
             if (mes.mediaIrradiacao) {
@@ -61,17 +59,17 @@ async function getData(latitude: number, longitude: number, consumo: number) {
         }, 0) / 12;
 
         const rendimentoInversor = inversores.inversor4.getEficiencia();
-        const potCorrigida = painel.calcularPotenciaCorrigida(Number(mediaTemperaturaAnual));
+        const potCorrigida = painel.calcularPotenciaCorrigida(mediaTemperaturaAnual);
         const potSaidaInversor = painel.calculaPotSaidaInversor(potCorrigida, rendimentoInversor, 1);
-        const energiaGerada = painel.calculoEnergiaGeradaMensal(30, Number(mediaIrradiacaoAnual) / 1000, 0.98, Number(potSaidaInversor));
+        const energiaGerada = painel.calculoEnergiaGeradaMensal(30,mediaIrradiacaoAnual/ 1000, 0.98, Number(potSaidaInversor));
 
 
-        totalInfo.mediaIrradiacaoAnual = Number(mediaIrradiacaoAnual.toFixed(2));
+        totalInfo.mediaIrradiacaoAnual = Math.round(mediaIrradiacaoAnual/1000);
         totalInfo.mediaPotCorrigida = Math.ceil(potCorrigida);
-        totalInfo.mediaTemperaturaAnual = Number(mediaTemperaturaAnual.toFixed(2));
-        const qtd = Math.ceil(consumo / Number(energiaGerada));
+        totalInfo.mediaTemperaturaAnual = Math.round(mediaTemperaturaAnual);
+        const qtd = Math.ceil(consumo / energiaGerada);
         totalInfo.qtdModulos = qtd;
-        totalInfo.potSistema = qtd * painel.potPico;
+        totalInfo.potSistema = (qtd * painel.potPico)/1000;
 
         const energiaGeradaMes = [];
         const mediaIrradiacao = [];
@@ -79,6 +77,7 @@ async function getData(latitude: number, longitude: number, consumo: number) {
         const meses = [];
         
         for (let i = 0; i < OpenMeteo.meses.length; i++) {
+
             const mes = OpenMeteo.meses[i];
             
             mediaIrradiacao.push(mes.mediaIrradiacao !== undefined ? mes.mediaIrradiacao : 0);
@@ -89,20 +88,20 @@ async function getData(latitude: number, longitude: number, consumo: number) {
 
             const energiaGerada = painel.calculoEnergiaGeradaMensal(mes.dias, mes.mediaIrradiacao !== undefined ? mes.mediaIrradiacao : 0 / 1000, 0.98, potSaidaInversor);
             energiaGeradaMes.push(energiaGerada);
-            // OpenMeteo.meses[i].energiaGerada = energiaGerada;
+            
         }
 
         totalInfo.energiaGeradaArray = { meses: meses, dados: energiaGeradaMes };
         totalInfo.mediaIrradiacao = { meses: meses, dados: mediaIrradiacao };
         totalInfo.mediaTemp = { meses: meses, dados: mediaTemp };
         const energiaTotal = totalInfo.energiaGeradaArray.dados.reduce((total, mes) => total + mes, 0);
-        //console.log(energiaTotal.toFixed(2))
-        totalInfo.energiaTotal = Number(energiaTotal.toFixed(2));
-        totalInfo.co2 = Number((energiaTotal * 0.295).toFixed(2));
-        totalInfo.arvores = Math.floor((totalInfo.co2 / 1000) * 7.14451202);
-        //totalInfo.dadosMes = OpenMeteo.meses;
-        totalInfo.areaInstalacao = Number((qtd * painel.areaInstalacao).toFixed(2));
-        //console.log(totalInfo)
+        
+        totalInfo.energiaTotal = Math.round(energiaTotal);
+        totalInfo.TONco2 = Math.round((energiaTotal * 0.295)/1000);
+        totalInfo.arvores = Math.round((totalInfo.TONco2) * 7.14451202);
+        
+        totalInfo.areaInstalacao = Math.round((qtd * painel.areaInstalacao));
+        
 
         return totalInfo;
     } catch (error) {
@@ -113,15 +112,14 @@ async function getData(latitude: number, longitude: number, consumo: number) {
 
 export default getData;
 
-//getData(-17, -50, 5000);
+// getData(-17, -50, 5000);
 
 // Para cada 1 kWh  = 0.295 co²
-//  1kWh = 0.00029499999999999996  = TON co² === 0.002107631045605 Arvores
+// 1kWh = 0.00029499999999999996  = TON co² === 0.002107631045605 Arvores
 //
 
 
 // 1189 kWh * 0.295 = 350.755 co²
-
-// (350.755/1000 ) * 7.14451202/1000  = 2.505973313224345 Arvores 
-
-
+//
+// (350.755/1000 ) * 7.14451202/1000  = 2.505973313224345 Arvores
+// 
